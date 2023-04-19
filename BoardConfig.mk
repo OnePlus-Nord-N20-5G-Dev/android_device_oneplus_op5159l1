876b9c745ae90547732c02bf741d41e0563eb5ff
Albert Tang <tangalbert919@yahoo.com>
2023-04-19 11:30:04 -0500

    gunnar: Replace side fps with udfps
    Change-Id: I03ede24c62c25d9ef5dbe0cced518703b4db758d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,9 @@ TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# Fingerprint
+TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
+
 # Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
 
